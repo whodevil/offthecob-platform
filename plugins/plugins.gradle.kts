8309438212fd922a5919c48e894ce273a526e9c7
whodevil <whodevil@offthecob.info>
2023-08-21 18:46:34 -0700

    Setup project for publishing to gradle plugin portal
    * Update the README with some useful documentation, annotated the plugins
    with useful information for when they are published.
    * Stop publishing plugins to github packages, publish them to gradle plugin portal
    instead.
    * Add gradle plugin publishing plugin to version catalog.

--- plugins/plugins.gradle.kts
+++ plugins/plugins.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     groovy
     `kotlin-dsl`
     alias(libs.plugins.spotless)
+    alias(libs.plugins.gradle.publish.plugin)
 }
 
 repositories {
@@ -19,23 +20,49 @@ java {
     }
 }
 
+group = "info.offthecob"
+version = System.getenv("REVISION") ?: "v9999999999"
+
 gradlePlugin {
+    website.set("https://github.com/whodevil/jvm-platform")
+    vcsUrl.set("https://github.com/whodevil/jvm-platform.git")
     plugins {
         create("base") {
             id = "info.offthecob.Base"
             implementationClass = "info.offthecob.gradle.BasePlugin"
+            tags.set(listOf("kotlin", "java", "groovy", "conventions"))
+            displayName = "Offthecob JVM Base"
+            description = "This plugin contains a bunch of best practices around how to build projects for the JVM. " +
+                    "This includes dependency locking by default, kotlin support, groovy support (for testing), wiring up the " +
+                    "junit platform, null away, and linting."
         }
         create("service") {
             id = "info.offthecob.Service"
             implementationClass = "info.offthecob.gradle.ServicePlugin"
+            tags.set(listOf("kotlin", "java", "groovy", "conventions", "jib", "containers"))
+            displayName = "Offthecob JVM Service"
+            description =
+                "This plugin contains all the work from `info.offthecob.Base` but includes `jib` for building " +
+                        "containers."
         }
         create("library") {
             id = "info.offthecob.Library"
             implementationClass = "info.offthecob.gradle.LibraryPlugin"
+            tags.set(listOf("kotlin", "java", "groovy", "conventions", "library"))
+            displayName = "Offthecob JVM Library"
+            description =
+                "This plugin contains all the work from `info.offthecob.Base` but includes `java-library` for " +
+                        "building jvm libraries."
         }
         create("springService") {
             id = "info.offthecob.SpringService"
             implementationClass = "info.offthecob.gradle.SpringService"
+            tags.set(listOf("kotlin", "java", "groovy", "conventions", "jib", "containers", "spring"))
+            displayName = "Offthecob JVM Spring Service"
+            description =
+                "This plugin contains all the work from `info.offthecob.Service` but includes customization of " +
+                        "the dependency management plugin used by spring, and applies the `kotlin-spring` and `spring-boot` " +
+                        "plugins."
         }
     }
 }
@@ -62,9 +89,21 @@ tasks {
     named("testClasses") {
         dependsOn("pluginUnderTestMetadata")
     }
-
+    named("publishPlugins") {
+        dependsOn("handlePluginPublishSecrets")
+    }
     withType<Test>().configureEach {
         useJUnitPlatform()
+    }
+    register("handlePluginPublishSecrets") {
+        doLast {
+            val key = System.getenv("GRADLE_PUBLISH_KEY")
+                ?: throw GradleException("Attempting to publish plugin without GRADLE_PUBLISH_KEY set.")
+            val secret = System.getenv("GRADLE_PUBLISH_SECRET")
+                ?: throw GradleException("Attempting to publish plugin without GRADLE_PUBLISH_SECRET set.")
+            System.setProperty("gradle.publish.key", key)
+            System.setProperty("gradle.publish.secret", secret)
+        }
     }
 }
 
