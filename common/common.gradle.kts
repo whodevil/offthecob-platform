f057fc0bb331829f099d9eed9e02f9ad1575d351
whodevil <whodevil@offthecob.info>
2023-08-25 16:31:47 -0700

    Try uploading the common jar to maven central
    I'm having an issue uploading the catalog project to maven, and I'm
    wondering if it's because I'm uploading a toml file rather than a jar.
    Ultimately, I want to have this common jar up there anyway, so let's see
    if we can get that working first.

--- common/common.gradle.kts
+++ common/common.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     `java-library`
+    signing
 }
 
 java {
     withJavadocJar()
     withSourcesJar()
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -14,6 +15,36 @@ configure<PublishingExtension> {
     publications {
         register<MavenPublication>("Common") {
             from(components["java"])
+            pom {
+                name.set("Common")
+                description.set("Common library supporting info.offthecob.platform")
+                url.set("https://github.com/whodevil/jvm-platform")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/license/mit/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("whodevil")
+                        name.set("Devon Gleeson")
+                        email.set("whodevil@offthecob.info")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/whodevil/jvm-platform.git")
+                    developerConnection.set("scm:git:https://github.com/whodevil/jvm-platform.git")
+                    url.set("https://github.com/whodevil/jvm-platform")
+                }
+            }
         }
     }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["Common"])
 }
