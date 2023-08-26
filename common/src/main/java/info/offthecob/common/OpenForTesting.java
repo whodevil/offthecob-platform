63fe74e5f88bd05c10fae2ed39ab0778bcbb2ac8
whodevil <whodevil@offthecob.info>
2023-08-25 17:11:56 -0700

    update javadoc for openfortesting

--- common/src/main/java/info/offthecob/common/OpenForTesting.java
+++ common/src/main/java/info/offthecob/common/OpenForTesting.java
@@ -1,4 +1,10 @@
 package info.offthecob.common;
 
+/**
+ * Wire this up to the kotlin all open compiler plugin to enable
+ * groovy mocking of kotlin classes in spock. The default behavior in
+ * kotlin is that kotlin classes are final in java bytecode, this causes
+ * spock framework mocking to fail.
+ */
 public @interface OpenForTesting {
 }
