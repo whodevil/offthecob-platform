bec9b15b077b1696b8bab90f87940291084e84ed
whodevil <whodevil@offthecob.info>
2024-06-02 13:51:48 -0700

    Use the foojay convention plugin (#131)
    This is the newer plugin and doesn't require direct interaction with the
    toolchain anymore

--- plugins/src/main/kotlin/info/offthecob/gradle/SettingsPlugin.kt
+++ plugins/src/main/kotlin/info/offthecob/gradle/SettingsPlugin.kt
@@ -3,9 +3,7 @@ package info.offthecob.gradle
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
-import org.gradle.kotlin.dsl.jvm
-import org.gradle.toolchains.foojay.FoojayToolchainResolver
-import org.gradle.toolchains.foojay.FoojayToolchainsPlugin
+import org.gradle.toolchains.foojay.FoojayToolchainsConventionPlugin
 import org.slf4j.LoggerFactory
 import java.io.File
 
@@ -62,15 +60,6 @@ class SettingsPlugin : Plugin<Any> {
     private fun subDirectoriesNotNamedBuildSrc(): (File) -> Boolean = { it.isDirectory and !it.name.contains(BUILD_SRC) }
 
     private fun toolChainSupport(settings: Settings) {
-        settings.plugins.apply(FoojayToolchainsPlugin::class.java)
-        settings.toolchainManagement.apply {
-            jvm {
-                javaRepositories {
-                    repository("foojay") {
-                        resolverClass.set(FoojayToolchainResolver::class.java)
-                    }
-                }
-            }
-        }
+        settings.plugins.apply(FoojayToolchainsConventionPlugin::class.java)
     }
 }
